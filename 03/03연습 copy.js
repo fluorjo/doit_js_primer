7b60d38a9be63cc0c9ee8843281a5cabd1160b6c
fluorjo <fluorjo@naver.com>
2022-08-07 22:51:35 +0900

    220807

---
+++ 03/03연습 copy.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = a++ +5; //증감 연산자는 피연산자 '뒤'에 있을 때 전체 수식의 처리가 끝난 다음에 적용. 반대로 앞에 있을 땐 전체 수식을 처리하기 전에 적용.
+//a++ a값에다 5를 더해서 b에 할당. 그 다음에 a를 1 증가시킴.  그래서 여기에서는 b는 15.
+var c = b--;
+//b가 14로 바뀜. c는 b의 15값을 받음. 
+var d = 15
+var e = ++d +5;
+//
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
---
+++ 03/03연습.js
@@ -0,0 +1,15 @@
+var a= 'hello'
+console.log(a);
+
+var seasons=['봄','여름','가을','겨울']
+console.log(seasons[0]);
+
+var kim = {
+    firstName:'john',
+    lastName:'kim',
+    age:35,
+    address:'seoul'
+}
+
+console.log(typeof kim);
+console.log(kim);
