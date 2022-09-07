c99c6d77cf98989d8a6541a5a448e260b4fbc90b
fluorjo <fluorjo@naver.com>
2022-09-07 13:17:12 +0900

    2209071

--- 05/js/event-dom.js
+++ 05/js/event-dom.js
@@ -2,3 +2,9 @@ var coverImage=document.querySelector('#cover');
 coverImage.onclick=function(){
     alert('누름');
 }   ;
+coverImage.onmouseover=function(){
+    coverImage.style.border='5px black solid';//사진에 검은색 테두리 그리기.
+    };  
+coverImage.onmouseout=function(){
+    coverImage.style.border='';//마우스 치우면 테두리 없애기
+    };
---
+++ 06/js/6.객체_연습1.js
@@ -0,0 +1,8 @@
+if (typeof window === "undefined") {
+    console.log("Oops, `window` is not defined")
+  }
+console.log(navigator.vendor);
+console.log(navigator.vendor);
+console.log(window);
+window.alert("안녕");
+console.log(alert('window 객체의 alert 함수 사용'));
