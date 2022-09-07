bae39a408fc524fdeed54f4bf71d4e24ba0d6213
fluorjo <82512693+fluorjo@users.noreply.github.com>
2022-09-07 13:16:44 +0900

    Delete 6.객체_연습2.js

--- 06/js/6.객체_연습2.js
+++
@@ -1,6 +0,0 @@
-var now=new Date()
-console.log(now)
-console.log(now.toLocaleString()) //현재 날짜와 시간 정보를 로컬 형식(현재 지역에 맞는 형식)으로 바꿔주는 함수
-//https://developer.mozilla.org/ko/docs/web/javascript/reference/global_objects
-//왼쪽에 Built-in objects 클릭.
-console.log(Math.random())
