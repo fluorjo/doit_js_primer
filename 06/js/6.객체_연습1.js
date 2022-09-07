f965b2bb942518e29dcd3269419759372ab6504b
fluorjo <82512693+fluorjo@users.noreply.github.com>
2022-09-07 13:16:50 +0900

    Delete 6.객체_연습1.js

--- 06/js/6.객체_연습1.js
+++
@@ -1,8 +0,0 @@
-if (typeof window === "undefined") {
-    console.log("Oops, `window` is not defined")
-  }
-console.log(navigator.vendor);
-console.log(navigator.vendor);
-console.log(window);
-window.alert("안녕");
-console.log(alert('window 객체의 alert 함수 사용'));
